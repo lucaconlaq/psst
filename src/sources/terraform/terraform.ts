83ca72f2b706b5d3129f9bf8c0e2c4114d7025c8
Luca <luca.querella@gmail.com>
2025-04-25 20:09:48 +0200

    First simple implementation of terraform fetcher

--- src/sources/terraform/terraform.ts
+++ src/sources/terraform/terraform.ts
@@ -2,6 +2,7 @@ import { SecretFetchResult, SecretFetchResultType } from "../../types.js";
 import { execSync } from "child_process";
 import { TerraformEditor } from "./TerraformEditor.js";
 import { SecretConfig, SecretSource } from "../../types.js";
+import { join } from "path";
 
 export interface TerraformSource extends SecretSource {
   executable: string;
@@ -15,7 +16,7 @@ export const terraformSource: TerraformSource = {
   referenceName: "Terraform path",
   executable: "terraform",
   versionCommand: "--version",
-  readCommand: "output",
+  readCommand: "output -json",
   fetchSecret: async function (
     secret: SecretConfig,
     key: string,
@@ -38,10 +39,34 @@ export const terraformSource: TerraformSource = {
         warning: `Invalid Terraform reference for [${key}].`,
       };
     }
-    return {
-      type: SecretFetchResultType.Success,
-      value: "output",
-    };
+
+    try {
+      const terraformPath = secret.value.split("#")[0];
+      const terraformItem = secret.value.split("#")[1];
+      const command = `${this.executable} ${this.readCommand}`;
+      const fullPath = join(configPath, terraformPath);
+
+      const result = execSync(command, {
+        encoding: "utf8",
+        cwd: fullPath,
+      }).trim();
+
+      const json = JSON.parse(result);
+      const value = json[terraformItem]["value"];
+
+      if (!value) {
+        return {
+          type: SecretFetchResultType.Warning,
+          warning: `Impossible to read secret from Terraform output [${secret.value}]`,
+        };
+      }
+      return { type: SecretFetchResultType.Success, value };
+    } catch (error) {
+      return {
+        type: SecretFetchResultType.Warning,
+        warning: `Impossible to read secret from Terraform output [${secret.value}]`,
+      };
+    }
   },
   editorOptions: [{ key: "←", description: "go back" }],
   renderEditor: TerraformEditor,
